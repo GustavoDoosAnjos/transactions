a96c2c9f4353770181e42e1e6e56f422c24b2f83
GustavoDoosAnjos <anjos.gu@gmail.com>
2025-01-23 18:46:18 -0300

    feat: Adiciona endpoint de estatistica

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,10 @@ app.get('/', (req, res) => {
 app.post('/transacao', async (req, res) => {
   const transaction = req.body;
 
+  // TODO VALIDAÇÕES
+
   if (!transaction?.valor || !transaction?.dataHora) {
-    return res.status(400).send('Valor e data são obrigatórios.');
+    return res.status(422).send();
   }
 
   try {
@@ -37,9 +39,43 @@ app.post('/transacao', async (req, res) => {
 
     await fs.writeFile(filePath, JSON.stringify(transactions, null, 2), 'utf8');
 
-    res.status(201).send('Transaction created successfully.');
+    res.status(201).send();
   } catch (error) {
-    res.status(500).send('Internal Server Error.');
+    res.status(400).send();
+  }
+});
+
+app.get('/estatistica', async (req, res) => {
+  const filePath = 'db/file.json';
+
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const transactions = JSON.parse(data);
+
+    const statistics = {
+      count: 0,
+      sum: 0,
+      avg: 0,
+      min: Infinity,
+      max: 0,
+    };
+
+    if (transactions.length > 0) {
+      statistics['count'] = transactions.length;
+      transactions.map((item) => {
+        statistics['sum'] += item.valor;
+        statistics['min'] = Math.min(statistics['min'], item.valor);
+        statistics['max'] = Math.max(statistics['min'], item.valor);
+        statistics['avg'] = statistics['sum'] / statistics['count'];
+      });
+    } else {
+      statistics['min'] = 0;
+    }
+
+    res.status(200).send(JSON.stringify(statistics));
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
   }
 });
 
